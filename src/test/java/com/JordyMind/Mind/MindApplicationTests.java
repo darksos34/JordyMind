98902b080abb0c4bf16acdd4d31e07065efa4e27
Your Name <j.hamwijk34@gmail.com>
2020-10-30 09:07:48 +0100

    Create MindApplicationTests.java

---
+++ src/test/java/com/JordyMind/Mind/MindApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JordyMind.Mind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MindApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
