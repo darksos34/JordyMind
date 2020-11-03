df919fc4bdb0eba055815e9f47c86b71a6c432a8
Your Name <j.hamwijk34@gmail.com>
2020-11-03 13:12:29 +0100

    Create SearchForm.js

---
+++ client/src/Profile/SearchForm.js
@@ -0,0 +1,25 @@
+import React, {useState} from "react";
+const SearchForm = () => {
+
+    const [inputText,setInputText] = useState("");
+
+    const [historyList,setHistoryList] = useState([]);
+    return (
+        <div>
+            <input onChange={(e) => {
+                setInputText(e.target.value)
+                setHistoryList([...historyList,e.target.value]);
+            }}
+                   placeholder="Enter some Text"/><br/>
+            {inputText}
+            <hr/><br/>
+            <ul>
+                {historyList.map((rec)=>{
+                    return<div>{rec}</div>
+                })}
+            </ul>
+        </div>
+
+    );
+}
+export default SearchForm;
