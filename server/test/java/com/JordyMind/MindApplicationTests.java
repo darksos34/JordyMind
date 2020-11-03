7c47c5b24ddcc816bf7bf98532ae0e00c343bf36
Your Name <j.hamwijk34@gmail.com>
2020-11-03 12:58:28 +0100

    Update MindApplicationTests.java

--- server/test/java/com/JordyMind/MindApplicationTests.java
+++ server/test/java/com/JordyMind/MindApplicationTests.java
@@ -1,11 +1,14 @@
 package test.java.com.JordyMind;
 
-import org.junit.jupiter.api.Test;
+
+import org.junit.Test;
+import org.junit.jupiter.api.Disabled;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MindApplicationTests {
 
+	@Disabled
 	@Test
 	void contextLoads() {
 	}
