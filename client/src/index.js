19192e0c12f760fbc943fd2d7dc77196bc86ff66
Your Name <j.hamwijk34@gmail.com>
2020-11-04 12:38:53 +0100

    Update index.js
    test

--- client/src/index.js
+++ client/src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import SearchForm from "./SearchProgram/SearchForm";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <SearchForm/>
   </React.StrictMode>,
   document.getElementById('root')
 );
