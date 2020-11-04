972a509bfb2cdb10c65108299ce814599a5f00a4
Your Name <j.hamwijk34@gmail.com>
2020-11-04 12:38:48 +0100

    Update App.js
    Trying

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
 import React from "react";
-import SearchForm from "./SearchProgram/SearchForm";
+
 const App = () => {
 
     return (
         <div>
-            <SearchForm/>
+
         </div>
 
     );
