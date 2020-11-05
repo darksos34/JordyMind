d67b643cb4fd8a4d62035430665b708967b126c6
Your Name <j.hamwijk34@gmail.com>
2020-11-05 15:59:41 +0100

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
 import React from "react";
+import ShowHideDetails from "./Profile/ShowHideDetails";
 
 const App = () => {
 
     return (
         <div>
+
+            <p>
+                Search and add a component to change Images.
+                For example when you hover over a picture that the color changes.
+            </p>
+            <ShowHideDetails/>
 
         </div>
 
