5cc6a86e56dbd53e8ccefa1c01e3b78b51bdcec0
Your Name <j.hamwijk34@gmail.com>
2020-11-05 15:07:33 +0100

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import React from "react";
+import ImageToggleOnScroll from "./ImageToggleOnScroll";
+import ImageTogglerOnMouseOver from "./ImageToggleOnMouseOver";
 
 const App = () => {
 
     return (
         <div>
-
+        <ImageToggleOnScroll/>
+        <ImageTogglerOnMouseOver/>
         </div>
 
     );
