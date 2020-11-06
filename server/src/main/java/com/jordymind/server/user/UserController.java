ddfef9bdf2769e315c71a78bb6219bb0dfa4939a
Your Name <j.hamwijk34@gmail.com>
2020-11-06 11:36:20 +0100

    Revert "Back-end update"
    This reverts commit a85f98167fd2878ad37a51f5a35feb9ae13550a1.

--- server/src/main/java/com/jordymind/server/user/UserController.java
+++ server/src/main/java/com/jordymind/server/user/UserController.java
@@ -1,6 +1,5 @@
 package com.jordymind.server.user;
 
-import lombok.NoArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,13 +9,18 @@ import java.util.Optional;
 
 
 
+
 @CrossOrigin(origins="http://localhost:3000", maxAge = 3600) // Cross origin allowed for port 3000
 @RestController // Instruct Spring Boot that this is the REST Controller
 @RequestMapping("/api/users") // Link to access GET/POST/PUT/DELETE requests
 public class UserController {
 
     // Import Methods from other Class
-    private  UserService userService; // Class from which methods are being imported
+    private final UserService userService; // Class from which methods are being imported
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 
 
     // Get all users
