965a5f418dd987029e11b2f849e9d52c3223ff27
Your Name <j.hamwijk34@gmail.com>
2020-11-06 08:50:52 +0100

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,19 +3,24 @@ import React from "react";
 import ShowHideDetails from "./Profile/ShowHideDetails";
 import Nav from "./Menu/Nav";
 
+
 const App = () => {
 
+
     return (
-        <>
-        <Nav/>
+
+        <main>
             <p>
+
                 Search and add a component to change Images.
                 For example when you hover over a picture that the color changes.
             </p>
+            <Nav/>
             <ShowHideDetails/>
 
 
-        </>
+        </main>
+
 
     );
 }
