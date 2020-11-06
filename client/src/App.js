7c105b7a2650e2e8c813d1d287cb5264d716b3cd
Your Name <j.hamwijk34@gmail.com>
2020-11-06 09:13:24 +0100

    Revert "Update App.js"
    This reverts commit e2a022c09dee83e8b5806d4b0f76e710b91efa92.

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,7 @@
 
 import React from "react";
 import ShowHideDetails from "./Profile/ShowHideDetails";
-import Home from "./Profile/Home";
-import SearchForm from "./SearchProgram/SearchForm";
+import Nav from "./Menu/Nav";
 
 
 const App = () => {
@@ -11,19 +10,13 @@ const App = () => {
     return (
 
         <main>
-            <Home/>
             <p>
 
                 Search and add a component to change Images.
                 For example when you hover over a picture that the color changes.
             </p>
-            <br/>
-
+            <Nav/>
             <ShowHideDetails/>
-            <br/>
-            This will be form to search on the entire webapp.
-            <SearchForm/>
-
 
 
         </main>
