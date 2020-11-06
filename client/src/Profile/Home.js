92d4704f4c7dcf5963c5c54decca4fd1b5841b74
Your Name <j.hamwijk34@gmail.com>
2020-11-06 08:57:58 +0100

    Update Home.js

--- client/src/Profile/Home.js
+++ client/src/Profile/Home.js
@@ -1,6 +1,7 @@
 import React  from "react";
 import { makeStyles } from '@material-ui/core/styles';
 import {Grid} from "@material-ui/core";
+import Nav from "../Menu/Nav";
 
 
 
@@ -15,6 +16,7 @@ const Home = () => {
 
     return (
         <div className={classes.root}>
+            <Nav/>
             <Grid container spacing={13}>
              <h1>Home Page</h1>
             </Grid>
